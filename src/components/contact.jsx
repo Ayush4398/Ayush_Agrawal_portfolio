8ba6c3c11e677aa3a2ded24a6641e1f43dfbdd59
Ayush4398 <agarwalayush1998@gmail.com>
2024-11-21 17:23:44 +0530

    Update contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -16,7 +16,6 @@ function contact(){
     <a href="mailto: agarwalayush1998@gmail.com"><img className="contacticon" src="doc/gmail.jpg" alt=""/></a>
     <a href="https://www.linkedin.com/in/ayush-agrawal-56b48b216/"><img className="contacticon" src="doc/linkedin.png" alt=""/></a>
     <a href="https://https://github.com/Ayush4398"><img className="contacticon" src="doc/github.png" alt=""/></a>
-    <a href="https://www.snapchat.com/add/agarwalayush04"><img className="contacticon" src="doc/snap.jpg" alt=""/></a>
     </div>
   </>
     );
